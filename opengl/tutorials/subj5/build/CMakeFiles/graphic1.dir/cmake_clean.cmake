502b38f55b04e29b97f165b7af9c7140d26ae752
mamadreza <mamad.rahmani235@gmail.com>
2026-09-04 11:33:54 -0400

    using glm and affecting on texture

---
+++ opengl/tutorials/subj5/build/CMakeFiles/graphic1.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graphic1.dir/link.d"
+  "CMakeFiles/graphic1.dir/main.cpp.o"
+  "CMakeFiles/graphic1.dir/main.cpp.o.d"
+  "CMakeFiles/graphic1.dir/shaders.cpp.o"
+  "CMakeFiles/graphic1.dir/shaders.cpp.o.d"
+  "CMakeFiles/graphic1.dir/src/glad.c.o"
+  "CMakeFiles/graphic1.dir/src/glad.c.o.d"
+  "graphic1"
+  "graphic1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/graphic1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ opengl/tutorials/subj5/fregment.fs
@@ -0,0 +1,17 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture;
+uniform sampler2D ourTexture2;
+uniform float visibility;
+
+void main()
+{
+    FragColor = mix(texture(ourTexture,TexCoord) , texture(ourTexture2, vec2(1.0 - TexCoord.x,TexCoord.y))
+    , 0.2+visibility) ;
+    
+}
+
